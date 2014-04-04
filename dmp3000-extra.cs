a9f02f94c4da3a60bf6b76fed8ea9f95123bdaf5
digwuren <digwuren@gmail.com>
2014-04-04 04:01:45 +0000

    added previously forgotten dmp3000-extra.cs

---
+++ dmp3000-extra.cs
@@ -0,0 +1,94 @@
+code 00 = U+2591
+code 01 = U+2592
+code 02 = U+2593
+code 03 = U+2502
+
+code 04 = U+2524
+code 05 = U+2510
+code 06 = U+252C
+code 07 = U+251C
+
+code 08 = U+253C
+code 09 = U+250C
+code 0A = U+2588
+code 0B = U+2584
+
+code 0C = U+258C
+code 0D = U+2590
+code 0E = U+2320
+code 0F = U+2518
+
+code 10 = U+2514
+code 11 = U+2534
+code 12 = U+2500
+code 13 = U+2580
+
+code 14 = U+03B1
+code 15 = U+0393
+code 16 = U+03C0
+code 17 = U+03A3
+
+code 18 = U+03C3
+code 19 = U+00B5
+code 1A = U+03C4
+code 1B = U+03A6
+
+code 1C = U+0398
+code 1D = U+03A9
+code 1E = U+03B4
+code 1F = U+221E
+
+code 20 = U+2208
+code 21 = U+2229
+code 22 = U+2261
+code 23 = U+00B1
+
+code 24 = U+2265
+code 25 = U+2264
+code 26 = U+2321
+code 27 = U+00F7
+
+code 28 = U+2248
+code 29 = U+00B0
+code 2A = U+2219
+code 2B = U+00B7
+
+code 2C = U+221A
+code 2D = U+207F
+code 2E = U+00B2
+code 2F = U+25A0
+
+code 30 = U+2665
+code 31 = U+2666
+code 32 = U+2663
+code 33 = U+2660
+
+code 34 = U+00C7
+code 35 = U+00E2
+code 36 = U+00EA
+code 37 = U+00EB
+
+code 38 = U+00EF
+code 39 = U+00EE
+code 3A = U+00F4
+code 3B = U+00FB
+
+code 3C = U+00FF
+code 3D = U+00A2
+code 3E = U+0192
+code 3F = U+00E1
+
+code 40 = U+00ED
+code 41 = U+00F3
+code 42 = U+00FA
+code 43 = U+00AA
+
+code 44 = U+00BA
+code 45 = U+2310
+code 46 = U+00AC
+code 47 = U+00BD
+
+code 48 = U+00BC
+code 49 = U+00AB
+code 4A = U+00BB
+code 4B = U+00DF
