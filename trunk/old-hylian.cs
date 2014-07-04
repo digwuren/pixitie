af8d002119f54195321f5982f04ca1ea7a044adb
digwuren@gmail.com <digwuren@gmail.com@255562bb-df61-035f-39c0-f835a86bd19f>
2014-07-04 19:14:43 +0000

    adding a draft font for the Old Hylian script

---
+++ trunk/old-hylian.cs
@@ -0,0 +1,59 @@
+# Old Hylian not being included in Unicode, we'll map it to katakana instead.
+
+code 20 = U+0020
+
+code 30 = U+30A2
+code 31 = U+30A4 
+code 32 = U+30A6
+code 33 = U+30A8
+code 34 = U+30AA
+
+code 35 = U+30AB
+code 36 = U+30AD
+code 37 = U+30AF
+code 38 = U+30B1
+code 39 = U+30B3
+
+code 3A = U+30B5
+code 3B = U+30B7
+code 3C = U+30B9
+code 3D = U+30BB
+code 3E = U+30BD
+
+code 3F = U+30BF
+code 40 = U+30C1
+code 41 = U+30C4
+code 42 = U+30C6
+code 43 = U+30C8
+
+code 44 = U+30CA
+code 45 = U+30CB
+code 46 = U+30CC
+code 47 = U+30CD
+code 48 = U+30CE
+
+code 49 = U+30CF
+code 4A = U+30D2
+code 4B = U+30D5
+code 4C = U+30D8
+code 4D = U+30DB
+
+code 4E = U+30DE
+code 4F = U+30DF
+code 50 = U+30E0
+code 51 = U+30E1
+code 52 = U+30E2
+
+code 53 = U+30E9
+code 54 = U+30EA
+code 55 = U+30EB
+code 56 = U+30EC
+code 57 = U+30ED
+
+code 58 = U+30E4
+code 59 = U+30E6
+code 5A = U+30E8
+
+code 5B = U+30EF
+code 5C = U+30F3
+code 5D = U+30F2
