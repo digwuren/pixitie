8ab17b12657b4f3c35eaaa7a931585c9edf1732d
digwuren <digwuren@gmail.com>
2014-07-04 14:38:09 +0000

    added the 7seg fonts

---
+++ 7seg.cs
@@ -0,0 +1,38 @@
+# ASCII subset
+code 20 = U+0020
+code 22 = U+0022
+code 27 = U+0027
+code 2D .. 39 = U+002D .. U+0039
+code 3D = U+003D
+code 3F = U+003F
+code 41 = U+0041
+code 43 = U+0043
+code 45 .. 4A = U+0045 .. U+004A
+code 4C = U+004C
+code 4E .. 53 = U+004E .. U+0053
+code 55 = U+0055
+code 59 = U+0059
+code 5B .. 66 = U+005B .. U+0066
+code 68 .. 6A = U+0068 .. U+006A
+code 6E = U+006E
+code 71 .. 72 = U+0071 .. U+0072
+code 74 .. 75 = U+0074 .. U+0075
+code 7E = U+007E
+
+# Some additional characters from Latin-1
+code B0 = U+00B0
+code F1 = U+00F1
+code F5 = U+00F5
+
+# Some additional characters from Unicode
+code 80 = U+02E5
+code 81 = U+02E7
+code 82 = U+02E9
+code 83 = U+0169
+code 85 = U+2261
+code 86 = U+2200
+code 87 = U+2234
+code 88 = U+2235
+code EC .. ED = U+03BB .. U+03BC
+code 89 .. 8C = U+2196 .. U+2199
+code 8D .. 8E = U+2921 .. U+2922
