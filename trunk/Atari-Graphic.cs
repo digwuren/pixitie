62fe987bd73de90df089b27ecd18c2e82b0c872f
digwuren@gmail.com <digwuren@gmail.com@255562bb-df61-035f-39c0-f835a86bd19f>
2014-07-05 23:38:14 +0000

    added Atari-GRaphic

---
+++ trunk/Atari-Graphic.cs
@@ -0,0 +1,53 @@
+include ascii.cs except 60, 7B, 7D, 7E
+
+code 00 = U+2665
+code 01 = U+251C
+code 03 = U+2518
+code 04 = U+2524
+code 05 = U+2510
+code 06 = U+2571
+code 07 = U+2572
+code 08 = U+25E2
+code 0A = U+25E3
+
+code 10 = U+2663
+code 11 = U+250C
+code 12 = U+2500
+code 13 = U+253C
+code 14 = U+2022
+code 17 = U+252C
+code 18 = U+2534
+code 1A = U+2514
+code 1C = U+2191
+code 1D = U+2193
+code 1E = U+2190
+code 1F = U+2192
+
+code 60 = U+2666
+
+code 7B = U+2660
+code 7D = U+21B0
+code 7E = U+25C0
+code 7F = U+25B6
+
+code 94 = U+25D8
+
+# autodetected 8*8 block graphics
+code 09 = U+2597
+code 0B = U+259D
+code 0C = U+2598
+code 0E = U+2582
+code 0F = U+2596
+code 15 = U+2584
+code 16 = U+258E
+code 19 = U+258C
+code 82 = U+258A
+code 89 = U+259B
+code 8B = U+2599
+code 8C = U+259F
+code 8D = U+2586
+code 8F = U+259C
+code 95 = U+2580
+code 99 = U+2590
+code A0 = U+2588
+# end of autodetection
