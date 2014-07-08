d0529ec5cf8bb02694f245c53badf7faaefe7067
digwuren@gmail.com <digwuren@gmail.com@255562bb-df61-035f-39c0-f835a86bd19f>
2014-07-08 16:56:28 +0000

    more letters, some improvements

--- trunk/14seg.cs
+++ trunk/14seg.cs
@@ -5,7 +5,6 @@ code 28 .. 2C = U+0028 .. U+002C
 code 3C = U+003C
 code 3E = U+003E
 code 40 = U+0040
-code 67 = U+0067
 code 42 = U+0042
 code 44 = U+0044
 code 4B = U+004B
@@ -14,30 +13,33 @@ code 54 = U+0054
 code 56 .. 58 = U+0056 .. U+0058
 code 59 = U+0059
 code 5A = U+005A
-code 6D = U+006D
+code 66 .. 67 = U+0066 .. U+0067
+code 6B .. 6D = U+006B .. U+006D
 code 6F = U+006F
 code 70 = U+0070
+code 73 = U+0073
 code 76 .. 77 = U+0076 .. U+0077
-code 7B .. 7D = U+007B .. U+007D
+code 7A .. 7D = U+007A .. U+007D
 
 # Some additional characters from Latin-1
 code E3 = U+00E3
 
 # Some additional characters from Unicode
+code 84 = U+03C5
 code 90 = U+0428
 code 91 = U+0448
 code 92 = U+042F
 code 93 = U+2203
 code 94 = U+1EBD
+code 96 = U+0418
+code 97 = U+042D
+code 98 = U+0416
+code 99 = U+0393
+code 9A -> U+002A
 code C4 = U+0394
 code C6 = U+03A6
 code CC = U+039B
 code D0 = U+03A0
 code D1 = U+03A8
-code 96 = U+0418
-code 97 = U+042D
-code 98 = U+0416
-code 99 = U+0393
 code D3 = U+03A3
 code E4 = U+03B4
-
